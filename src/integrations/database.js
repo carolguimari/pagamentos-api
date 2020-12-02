bc406c9a56cab7c2f2e0da6390bcd884001c5513
Carolina Guimarães <carolguimari@gmail.com>
2020-12-02 20:31:06 -0300

    fix: conexão

--- src/integrations/database.js
+++ src/integrations/database.js
@@ -3,7 +3,7 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.URI,
 	ssl: {
 		rejectUnauthorized: false,
 	},
