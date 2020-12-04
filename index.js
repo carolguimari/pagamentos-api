7ad14286334029e4e95fc27dc9195c198a573cba
Carolina Guimarães <carolguimari@gmail.com>
2020-12-03 21:29:23 -0300

    fix: cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
+const Cors = require('@koa/cors');
 
 const PORT = process.env.PORT || 8081;
 const server = new Koa();
 const router = require('./src/routes');
 
+server.use(Cors({ origin: '*' }));
 server.use(bodyparser());
 
 server.use(router.routes());
